c9fa7f92aaf829a1e843cb068e412ce6d8515b8e
emulk <orgest.shehaj@gmail.com>
2013-01-14 23:54:05 +0100

    Update SQL/inserimento/inserimento-vendite.sql

--- SQL/inserimento/inserimento-vendite.sql
+++ SQL/inserimento/inserimento-vendite.sql
@@ -1,4 +1,16 @@
 INSERT INTO vendita (veicolo, concessionario)
 VALUES
-  ('BMW i3', 12),
-	('Buddy', 17);
+  	('BMW i3', 12),
+	('Buddy', 17),
+	('i-Miev',11),
+	('WILL', 14),
+	('mia K', 11),
+	('i-Miev', 9),
+	('Nmg2',3),
+	('Carbon Zero', 18),
+	('Luciole', 22),
+	('ZEV Smiley',50 ),
+	('Wheego Whip', 42),
+	('RAV4 EV', 38),
+	('THINK City', 33),
+	('Tazzari Zero', 22);
