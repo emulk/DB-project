aa2976105a7096fcabcad688d5d23ed3f472e55d
emulk <orgest.shehaj@gmail.com>
2013-01-14 21:58:55 +0100

    recensioni

---
+++ SQL/inserimento/inserimento-recensioni.sql
@@ -0,0 +1,7 @@
+ISERT INTO recensione (veicolo, utente, data, likes, dislikes, voto, testo, posseduto)
+VALUES
+	('BMW i3', 'Orgest', '12/01/2013', 1, 0, 8, 'Questa e solo una prova', 0),
+	('E-MEGA', 'Marco', '04/01/2013', 1, 0 , 6, 'Questa macchina e fantastica', 0),
+	('Loremo EV', 'Donato', '12/12/2012', 0 , 0 , 'Ho comprato la macchina elettrica', 1),
+	('Mercedes Benz SLS eDrive EV', 'Donato', '24/12/2012',1, 0, 'La voglio anche io', 0),
+	('Buddy', 'Marco', '05/01/2013', 0, 1, 'Non la voglio', 0);
