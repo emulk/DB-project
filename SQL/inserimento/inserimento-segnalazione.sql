d76cddccfa31ab71ecbc3562e44f3147bedc051f
emulk <orgest.shehaj@gmail.com>
2013-01-15 00:00:11 +0100

    Update SQL/inserimento/inserimento-segnalazione.sql

--- SQL/inserimento/inserimento-segnalazione.sql
+++ SQL/inserimento/inserimento-segnalazione.sql
@@ -2,4 +2,7 @@ INSERT INTO segnalazione (colonnina, utente, data, utile, inutile, tipo, descriz
 VALUES
   (5, 'donato', '13/01/2013', 1, 0, 'positivo', 'installate nuove prese con tecnologia di ricarica rapida'),
   (8, 'marco','15/01/2013', 0, 0, 'negativo', 'non legge la carta di credito'),
-  (13, 'orgest', '15/01/2013', 1, 0, 'negativo', 'problema alla presa 5, non carica');
+  (13, 'orgest', '15/01/2013', 1, 0, 'negativo', 'problema alla presa 5, non carica'),
+  (22, 'paolo', '11/01/2013', 0, 0, 'positivo', 'sottocosto'),
+  (11, 'sebastiano', '05/01/2013', 1, 0, 'positivo', 'niente coda'),
+  (22, 'gianfranco', '01/01/2013', 0, 1, 'negativo', 'trovato chiuso per ferie');
