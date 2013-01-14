cb2f1eb8bba5cce3c3f4a4e98cb5dce9efd5b766
emulk <orgest.shehaj@gmail.com>
2013-01-14 23:42:52 +0100

    Update SQL/inserimento/inserimento-possesso.sql

--- SQL/inserimento/inserimento-possesso.sql
+++ SQL/inserimento/inserimento-possesso.sql
@@ -1,4 +1,13 @@
 INSERT INTO possesso (utente, veicolo)
 VALUES
 	('donato', 'Chevrolet Volt'),
-	('orgest', 'Twizy Z.E');
+	('orgest', 'Twizy Z.E'),
+	('andrea', 'A1 Sportback'),
+	('giulio','Invita 2-Seater'),
+	('paolo', 'Mini-E'),
+	('mattia','BMW i8'),
+	('gianfranco','e6 EV'),
+	('lorenzo', 'Citroen C-Zero'),
+	('sebastiano','E-MEGA'),
+	('matteo','WILL'),
+	('daniel','i-Miev');
