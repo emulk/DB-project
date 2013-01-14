4caf438729e0c52b8ea2766565ec6e83d1f08238
specialdo <specialdo@hotmail.it>
2013-01-14 22:37:55 +0100

    Aggiornata lista utenti

--- SQL/inserimento/inserimento-utente.sql
+++ SQL/inserimento/inserimento-utente.sql
@@ -3,3 +3,9 @@ VALUES
 	('marco', 'marco@unibo.it', 44.50203, 11.343801),
 	('donato', 'donato@unibo.it', 44.345826, 11.728223),
 	('orgest', 'orgest@unibo.it', 44.698494, 10.628451);
+	('andrea', 'andrea@unibo.it', 44.51380, 11.35849),
+	('giulia', 'giulia@unibo.it', 44.61578,10.93149);
+	('giuseppe', 'giuseppe@unibo.it', 44.14854, 12.07191),
+	('cristina', 'cristina@unibo.it', 44.65137, 10.58521);
+	('vito', 'vito@unibo.it', 44.73433, 11.29527),
+	('fabio', 'fabio@unibo.it', 44.69810, 11.80903);
