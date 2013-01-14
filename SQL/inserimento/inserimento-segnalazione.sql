4e7881551b18da2504934c1cc8ed6a7c3257d4d0
emulk <orgest.shehaj@gmail.com>
2013-01-14 13:16:46 -0800

    Create inserimento-segnalazione.sql

---
+++ SQL/inserimento/inserimento-segnalazione.sql
@@ -0,0 +1,5 @@
+INSERT INTO segnalazione (colonnina, utente, data, utile, inutile, tipo, descrizione)
+VALUES
+  (5, 'donato', '13/01/2013', 1, 0, 'descrizione', 'il prezzo piu' basso'),
+	(8, 'marco','15/01/2013', 0, 0, 'prova', 'e solo una prova'),
+	(13, 'orgest', '15/01/2013', 1, 0, 'prova2', ''continuo a provare');
