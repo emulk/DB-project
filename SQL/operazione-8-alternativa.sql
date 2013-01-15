6b78855e6a56031137dbe8f1e49b768da188be13
specialdo <specialdo@hotmail.it>
2013-01-15 14:12:31 +0100

    Query operazione 8 usando JOIN

---
+++ SQL/operazione-8-alternativa.sql
@@ -0,0 +1,8 @@
+﻿-- 8. Visualizzare i concessionari che vendono un particolare modello
+-- nel raggio di X kilometri dalla posizione di un certo utente
+
+SELECT id,nome,distance(latitudine, longitudine, LatUtente, LongUtente)AS Distanza
+FROM concessionario JOIN vendita ON concessionario.id=vendita.concessionario,(SELECT latitudine AS LatUtente, longitudine AS LongUtente
+		 FROM utente
+		 WHERE utente.username = 'donato') AS userPos		 
+WHERE distance (latitudine, longitudine, LatUtente, LongUtente)<100 AND veicolo = 'i-Miev'
